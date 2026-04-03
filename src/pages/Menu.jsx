b73b97e7ac95f2ceb09086db19649978c66c1fe0
Asad Naved <asadnaved4@gmail.com>
2026-04-04 03:30:53 +0530

    MenuContainer UI-2

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -3,13 +3,12 @@ import BottomNav from "../components/shared/BottomNav";
 import BackButton from "../components/shared/BackButton";
 import { MdRestaurantMenu } from "react-icons/md";
 import MenuContainer from "../components/menu/MenuContainer";
-// import CustomerInfo from "../components/menu/CustomerInfo";
-// import CartInfo from "../components/menu/CartInfo";
-// import Bill from "../components/menu/Bill";
+import CustomerInfo from "../components/menu/CustomerInfo";
+import CartInfo from "../components/menu/CartInfo";
+import Bill from "../components/menu/Bill";
 // import { useSelector } from "react-redux";
 
 const Menu = () => {
-
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
       {/* Left Div */}
@@ -26,11 +25,9 @@ const Menu = () => {
               <MdRestaurantMenu className="text-[#f5f5f5] text-4xl" />
               <div className="flex flex-col items-start">
                 <h1 className="text-md text-[#f5f5f5] font-semibold tracking-wide">
-                 Customer Name
+                  Customer Name
                 </h1>
-                <p className="text-xs text-[#ababab] font-medium">
-                  Table : N/A
-                </p>
+                <p className="text-xs text-[#ababab] font-medium">Table : 2</p>
               </div>
             </div>
           </div>
@@ -38,9 +35,16 @@ const Menu = () => {
         <MenuContainer />
       </div>
       {/* Right Div */}
-      <div className="flex-[1] bg-blue-500">
+      <div className="flex-[1] bg-[#1a1a1a] mt-4 mr-3 h-[780px] rounded-lg pt-2">
+        {/* Customer Info */}
+        <CustomerInfo />
+        <hr className="border-[#2a2a2a] border-t-2" />
+        {/* Cart Items */}
+        <CartInfo />
+        <hr className="border-[#2a2a2a] border-t-2" />
+        {/* Bill */}
+        <Bill />
       </div>
-      
 
       <BottomNav />
     </section>
