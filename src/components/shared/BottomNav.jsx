f824a8b52923739b4201173305cb1ccc1c8c200b
Asad Naved <asadnaved4@gmail.com>
2026-04-04 14:57:57 +0530

    Redux 1st Part Done✅

--- src/components/shared/BottomNav.jsx
+++ src/components/shared/BottomNav.jsx
@@ -6,12 +6,18 @@ import { CiCircleMore } from "react-icons/ci";
 import { BiSolidDish } from "react-icons/bi";
 import { useNavigate, useLocation } from "react-router-dom";
 import Modal from "./Modal";
+import { useDispatch } from "react-redux";
+import { setCustomer } from "../../redux/slices/customerSlice";
 
 const BottonNav = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const dispatch = useDispatch();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [guestCount, setGuestCount] = useState(0);
+  const [name, setName] = useState();
+  const [phone, setPhone] = useState();
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
@@ -26,34 +32,43 @@ const BottonNav = () => {
 
   const isActive = (path) => location.pathname === path;
 
+    const handleCreateOrder = () => {
+    // send the data to store
+    dispatch(setCustomer({name, phone, guests: guestCount}));
+    navigate("/tables");
+  }
+
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">
       <button
         onClick={() => navigate("/")}
-className={`flex items-center justify-center font-bold ${
+        className={`flex items-center justify-center font-bold ${
           isActive("/") ? "text-[#f5f5f5] bg-[#343434]" : "text-[#ababab]"
-        } w-[300px] rounded-[20px]`}      >
+        } w-[300px] rounded-[20px]`}
+      >
         <FaHome className="inline mr-2" size={20} /> <p>Home</p>
       </button>
       <button
         onClick={() => navigate("/orders")}
-className={`flex items-center justify-center font-bold ${
+        className={`flex items-center justify-center font-bold ${
           isActive("/orders") ? "text-[#f5f5f5] bg-[#343434]" : "text-[#ababab]"
-        } w-[300px] rounded-[20px]`}      >
+        } w-[300px] rounded-[20px]`}
+      >
         <MdOutlineReorder className="inline mr-2" size={20} /> <p>Orders</p>
       </button>
       <button
         onClick={() => navigate("/tables")}
-className={`flex items-center justify-center font-bold ${
+        className={`flex items-center justify-center font-bold ${
           isActive("/tables") ? "text-[#f5f5f5] bg-[#343434]" : "text-[#ababab]"
-        } w-[300px] rounded-[20px]`}      >
+        } w-[300px] rounded-[20px]`}
+      >
         <MdTableBar className="inline mr-2" size={20} /> <p>Tables</p>
       </button>
       <button className="flex items-center justify-center text-[#ababab] w-[200px]">
         <CiCircleMore className="inline mr-2" size={20} /> <p>More</p>
       </button>
       <button
-      disabled={isActive("/tables") || isActive("/menu")}
+        disabled={isActive("/tables") || isActive("/menu")}
         onClick={openModal}
         className="absolute bottom-6 bg-[#F6B100] text-[#f5f5f5] rounded-full p-4 items-center"
       >
@@ -66,6 +81,8 @@ className={`flex items-center justify-center font-bold ${
           </label>
           <div className="flex items-center rounded-lg p-3 px-4 bg-[#1f1f1f]">
             <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               type="text"
               name=""
               placeholder="Enter customer name"
@@ -80,6 +97,8 @@ className={`flex items-center justify-center font-bold ${
           </label>
           <div className="flex items-center rounded-lg p-3 px-4 bg-[#1f1f1f]">
             <input
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
               type="number"
               name=""
               placeholder="+91-9999999999"
@@ -103,7 +122,7 @@ className={`flex items-center justify-center font-bold ${
           </div>
         </div>
         <button
-          onClick={() => navigate("/tables")}
+          onClick={handleCreateOrder}
           className="w-full bg-[#F6B100] text-[#f5f5f5] rounded-lg py-3 mt-8 hover:bg-yellow-700"
         >
           Create Order
--- src/components/tables/TableCard.jsx
+++ src/components/tables/TableCard.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 // import { FaCheckDouble } from "react-icons/fa";
 import { getBgColor } from '../../utils';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { updateTable } from '../../redux/slices/customerSlice';
 
 
 const TableCard = ({key,name,status,initials,seats}) => {
-
+  const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleClick = () => {
+    const handleClick = (name) => {
        if(status === "Booked") return;
+       dispatch(updateTable({table: name}));
        navigate(`/menu`);
     }
 
   return (
-    <div onClick={handleClick} key={key} className="w-[300px] hover:bg-[#383838] bg-[#262626] p-4 rounded-lg cursor-pointer">
+    <div onClick={() => handleClick(name)} key={key} className="w-[300px] hover:bg-[#383838] bg-[#262626] p-4 rounded-lg cursor-pointer">
       <div className="flex items-center justify-between px-1">
         <h1 className='text-[#f5f5f5] text-xl font-semibold'>{name}</h1>
         <p className={`px-2 py-1 rounded-lg ${status === "Booked" ? "bg-[#2e4a40] text-green-600" : "bg-[#664a04] text-white"}`}> {status} </p>
